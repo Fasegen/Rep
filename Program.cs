6ae29e4bba4b7c759263488da57b172dcc1a770d
unknown <floidslave@mail.ru>
2022-09-21 21:40:52 +0300

    123

--- Program.cs
+++ Program.cs
@@ -1,38 +1,79 @@
-﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
-
-Console.Write("Введите k1: ");
-var k1 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите b1: ");
-var b1 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите k2: ");
-var k2 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите b2: ");
-var b2 = Convert.ToDouble(Console.ReadLine());
-
-var x = (b2 - b1) / (k1 - k2);
-var y = k1 * x + b1;
- 
-x = Math.Round(x, 4);
-y = Math.Round(y, 4);
- 
-Console.WriteLine($"Пересечение в точке: ({x};{y})");
+﻿
 
 
-// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
 
-Console.Write("Введите элементы через запятую: ");
-int[] arr = Array.ConvertAll(Console.ReadLine().Split(","), int.Parse);
-int count = 0;
- 
-for (int i = 0; i < arr.Length; i++)
+Console.WriteLine("Введите размеры массива");
+Random rd = new Random();
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] A = new double[n, m];
+
+for (int i = 0; i < A.GetLength(0); i++)
 {
-    if (arr[i] < 0)
+    for (int j = 0; j < A.GetLength(1); j++)
     {
-        count++;
+        A[i, j] = rd.Next(-100, 100);
+        Console.Write($"{A[i, j]} ");
     }
+    Console.WriteLine();
 }
+
+
+// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+Console.WriteLine("Введите размеры массива");
+Random rd = new Random();
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] A = new int[n, m];
+
+for (int i = 0; i < A.GetLength(0); i++)
+{
+    for (int j = 0; j < A.GetLength(1); j++)
+    {
+        A[i, j] = rd.Next(1, 10);
+        Console.Write($"{A[i, j]} ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("Введите координаты");
+ int a = Convert.ToInt32(Console.ReadLine());
+ int b = Convert.ToInt32(Console.ReadLine());
+ if (a>=m && b>=n)
+ Console.WriteLine("такого числа нет");
+ else
+ {
+ object c = A.GetValue(a,b);
+ Console.WriteLine(c);
+ }
+
  
-Console.WriteLine(count);
 
+// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
 
--
+Console.WriteLine("Введите размеры массива");
+Random rd = new Random();
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] A = new int[n, m];
+
+for (int i = 0; i < A.GetLength(0); i++)
+{
+    for (int j = 0; j < A.GetLength(1); j++)
+    {
+        A[i, j] = rd.Next(1, 10);
+        Console.Write($"{A[i, j]} ");
+    }
+    Console.WriteLine();
+}
+
+for (int i = 0; i < A.GetLength(0); i++)
+{
+    double count = 0;
+    for (int j = 0; j < A.GetLength(1); j++)
+    {
+        count += A[j, i];
+    }
+    Console.WriteLine(count/n);
+}
